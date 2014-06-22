26e9f461688596486d64e48e8e17840d03c4af37
root <root@yadev.ru>
2014-06-23 02:01:47 +0400

    update

--- index.php
+++ index.php
@@ -1,35 +1,46 @@
-<style>
-	input {display:block;}
-</style>
-
-<form method="post">
-	<input type="text" name="name" />
-	<input type="text" name="phone" />
-	<input type="text" name="email" />
-	<input type="submit" name="Отправить" />
-</form>
-
 <?php
 
-	$message = '';
-
-	$fields = array
-	(
-		'name'	=> 'Имя: ',
-		'phone' => 'Телефон: ',
-		'email' => 'Почта: '
-	);
+class TextHndler
+{
+	const nl = '<br/>';
 	
+	public $text = '';
 	
-	if(!empty($_POST))
+	public $input = array();
+	
+	public $fields = array();
+	
+	public function __construct($input, $fields = array())
 	{
-		foreach($_POST as $field => $value)
+		$this->input = $input;
+		$this->fields = $fields;
+	}
+	
+	public function build()
+	{
+		if(!is_array($this->input) || empty($this->input) && !is_array($this->fields) || empty($this->fields))
+			return false;
+			
+		foreach($this->input as $field => $value)
 		{
-			if(array_key_exists($field, $fields))
+			if(array_key_exists($field, $this->fields))
 			{
-				$message .= $fields[$field] . $value . '<br/>';
+				$this->text .= $this->fields[$field] . $value . self::nl;
 			}
 		}
+		return $this->text;
 	}
-	echo $message;
+}
+
+$fields = array
+(
+	'name'	=> 'Имя: ',
+	'phone' => 'Телефон: ',
+	'email' => 'Почта: '
+);
+
+
+$text = new TextHandler($_POST, $fields);
+
+echo $text->build();
 ?>
